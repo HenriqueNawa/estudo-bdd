6169e2f447a0ae5ef6ab2e7444eb17d26bc8732c
HenriqueNawa <henriquenawa@outlook.com>
2023-02-05 23:02:21 -0300

    escrita cenário feature calculo

---
+++ estudoBDD/src/test/java/estudoBDD/CalculoTest.java
@@ -0,0 +1,24 @@
+package estudoBDD;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class CalculoTest {
+	
+	@Dado("que o usuário escolha a opção de adição")
+	public void que_o_usuário_escolha_a_opção_de_adição() {
+	    
+	}
+
+	@Quando("o usuário entrar com os valores {int} e {int}")
+	public void o_usuário_entrar_com_os_valores_e(Integer int1, Integer int2) {
+	    
+	}
+
+	@Então("o resultado será {int}")
+	public void o_resultado_será(Integer int1) {
+	    
+	}
+
+}
---
+++ estudoBDD/src/test/resources/calculo.feature
@@ -0,0 +1,7 @@
+#language: pt
+Funcionalidade: Operações matemáticas basicas
+
+  Cenário: realizar a soma de dois números
+    Dado que o usuário escolha a opção de adição
+    Quando o usuário entrar com os valores 1 e 1
+    Então o resultado será 2
